b9a323602f96197f7c185bef1dfd86903d47fd45
Turner Silverthorne <turner.silverthorne@gmail.com>
2024-06-28 10:59:30 -0400

    Initial repo setup, mainly renv changes

---
+++ R/evalExactPower.R
@@ -0,0 +1,51 @@
+#' Evaluate exact power of harmonic regression hypothesis test.
+#' 
+#' @description
+#' Returns exact power of harmonic regression hypothesis test. 
+#' 
+#' 
+#' @param t measurement schedule
+#' @param param$Amp amplitude of signal 
+#' @param param$freq frequency of signal
+#' @param param$acro phase of signal in radians
+#' @param alpha type I error, by default \code{alpha=.05}
+#' 
+#' @note
+#' Assumes the noise has mean zero and unit standard deviation.
+#' In these units, amplitude can be interpreted as the signal to noise ratio.
+#' 
+#' @return statistical power 
+#' @author Turner Silverthorne
+#' @export
+evalExactPower <- function(t,param,alpha=.05,method='schur'){
+# return power of one-frequency cosinor model
+  Amp    = param[['Amp']]
+  freq   = param[['freq']]
+  acro   = param[['acro']]
+  N      = length(t)
+  
+  if (method=='full'){
+    A      = matrix(c(0,0,1,0,0,1),nrow=3,byrow=T)
+    X      = matrix(c(rep(1,N),cos(2*pi*freq*t),sin(2*pi*freq*t)),ncol=3)
+    B      = t(A)%*%solve(t(X)%*%X,A)
+    beta   = matrix(c(Amp*cos(acro),Amp*sin(acro)),nrow=2)
+    lambda = t(beta)%*%solve(B,beta)
+  }else if (method=='schur'){
+    A      = matrix(c(0,0,1,0,0,1),nrow=3,byrow=T)
+    Xr     = matrix(c(cos(2*pi*freq*t),sin(2*pi*freq*t)),ncol=2)
+    D      = t(Xr)%*%Xr
+    b      = matrix(c(sum(cos(2*pi*freq*t)),sum(sin(2*pi*freq*t))),ncol=1)
+    invB   = D - b%*%t(b)/N
+    beta   = matrix(c(Amp*cos(acro),Amp*sin(acro)),nrow=2)
+    lambda = t(beta)%*%invB%*%beta
+  }else if (method=='old'){
+    cvec   = Amp*cos(2*pi*freq*t-acro)
+    lambda = as.numeric(t(cvec)%*%cvec)
+  }else{
+    stop('unknown method')
+  }
+  
+  f0     = qf(p=1-alpha,df1=2,df2=N-3)
+  return(1 - pf(q=f0,df1=2,df2=N-3,ncp=lambda))
+}
+
---
+++ R/getPcQuadCsts.R
@@ -0,0 +1,37 @@
+getPcQuadCsts=function(xfreq,fmin,fmax,Nfreq,Nfine){
+  QC=list() # list of quadratic constraints
+  
+  # matrices for regularization term 
+  tau  = c(1:Nfine)/Nfine -1/Nfine 
+  fvec = seq(from=fmin,to=fmax,length.out=Nfreq)
+  
+  for (ii in c(1:Nfreq)){
+    xf     = xfreq[ii] 
+    freq   = fvec[ii]
+    cvec   = cos(2*pi*freq*tau)
+    svec   = sin(2*pi*freq*tau)
+    
+    C1top  = cbind(diag(cvec*cvec),diag(cvec*svec))
+    C1bot  = cbind(diag(cvec*svec),diag(svec*svec))
+    C1     = rbind(C1top,C1bot)
+     
+    C2top  = cbind(outer(cvec,cvec,'*'),outer(cvec,svec,'*'))
+    C2bot  = cbind(outer(cvec,svec,'*'),outer(svec,svec,'*'))
+    C2     = rbind(C2top,C2bot)
+     
+    Cmat   = C1-C2/Nfine
+     
+    Cm11   = Cmat[1:Nfine,1:Nfine]
+    Cm12   = Cmat[1:Nfine,(Nfine+1):(2*Nfine)]
+    Cm21   = Cmat[(Nfine+1):(2*Nfine),1:Nfine]
+    Cm22   = Cmat[(Nfine+1):(2*Nfine),(Nfine+1):(2*Nfine)]
+   
+    qcmat  = xf*Cm11 + (1-xf)*Cm22 + sqrt(xf)*sqrt(1-xf)*(Cm12+Cm21)
+    qcmat  = cbind(rbind(qcmat,rep(0,Nfine)),rep(0,Nfine+1))
+    qc     = c(rep(0,Nfine),-1)
+    beta   = 0
+    sense  = '>'
+    QC[[ii]] = list(Qc=qcmat,q=qc,rhs=beta,sense=sense)
+  }
+  return(QC)
+} 
