10b976307e1fd2a11ffd34ee831739f21c102d28
Turner Silverthorne <turner.silverthorne@gmail.com>
2024-07-12 11:49:18 -0400

    YALMIP sweep matlab code

--- matlab/pCHORD.m
+++ matlab/pCHORD.m
@@ -1,13 +1,23 @@
-function [mu,eta] = pCHORD(n,Nm,fvec,options)
+function [prob,mu,eta] = pCHORD(n,Nm,fvec,options,relax)
 arguments % can leave options blank and yet YALMIP decide on everything
     n;Nm;fvec;
-    options=sdpsettings();
+    options = sdpsettings();
+    relax   = false;
 end
 
 sdpvar eta; % decision variables
-mu = binvar(n,1);
+if relax
+    mu = sdpvar(n,1);
+else
+    mu = binvar(n,1);
+end
+   
+
 
 F  = sum(sum(mu))==Nm; % constraints
+if relax
+    F = [F,mu>=0,mu<=1]
+end
 for freq=fvec
     [Cm11,Cm12,Cm21,Cm22] = getFourQuadBlocks(n,Nm,freq);
     F = [F, eta <=lambda_min([mu'*Cm11*mu mu'*Cm12*mu; mu'*Cm21*mu mu'*Cm22*mu])];
@@ -16,7 +26,7 @@ end
 h =-eta; % objective to be minimized (YALMIP only does minimzation)
 
 warning('off','all') % TODO: make this optional in future
-optimize(F,h,options) % solve
+prob=optimize(F,h,options);% solve
 warning('on','all')
 end
 
---
+++ matlab/sweep_yalmip.m
@@ -0,0 +1,35 @@
+mkdir 'yalmip_sweep'
+fmin = 1:2:12;
+fmax = 1:2:24;
+df   = .5;
+n    = 144;
+[fmin,fmax,Nmeas]=ndgrid(fmin,fmax,[16,24,32,48]);
+
+pars=[fmin(:) fmax(:) Nmeas(:)];
+
+
+maxT=60;
+options = sdpsettings('solver','bmibnb', ...
+                    'bmibnb.uppersolver','fmincon', ...   % can't be mosek
+                    'bmibnb.lowersolver','mosek', ...   % has to be mosek
+                    'bmibnb.lpsolver','mosek', ... % can be gurobi or mosek
+                    'bmibnb.maxtime',maxT, ...
+                    'bmibnb.maxiter',Inf);
+
+ii      = str2double(getenv('SLURM_ARRAY_TASK_ID'));
+fmin    = pars(ii,1);
+fmax    = pars(ii,2);
+Nmeas   = pars(ii,3);
+fvec    = (fmin:df:fmax);
+
+fname = strcat('yalmip_sweep/', ...
+               'Nmeas_',num2str(Nmeas),...
+               '_fmin_',num2str(fmin),...
+               '_fmax_',num2str(fmax),...
+               '_maxT_',num2str(maxT),...
+               '.mat');
+
+prob    = pCHORD(n,Nmeas,fvec,options);
+res     = {fmin,fmax,Nmeas,prob};
+
+save(fname,'res')
