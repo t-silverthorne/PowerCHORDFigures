27c2438af712478d08c696d77812d27febc7b761
Turner Silverthorne <turner.silverthorne@gmail.com>
2024-07-22 19:47:31 -0400

    Periodogram robustness code

--- figures/sec3p2_spectral_auc.R
+++ figures/sec3p2_spectral_auc.R
@@ -1,60 +1,54 @@
 require(pROC)
-
+require(lomb)
+freq_vals = seq(20,30,.1)
+Nmc       = 1e3
+mc_cores  = 8
 pars       = expand.grid(freq=freq_vals,
-                         Nmeas=c(32,40,48),
+                         Nmeas=c(48),
                          Amp = c(1),
                          p_osc = c(0.5),
-                         fdr_method=c('none'),
-                         stat_method=c('cosinor'),
-                         type=c('equispaced','threshold','balanced','regu_no_cstr'))
+                         stat_method=c('lomb'),
+                         type=c('equispaced','WCP'))
 
-
-#pars=rbind(pars,pars,pars) # run 3 copies so you can compute sdev
 dim(pars)
-df=c(1:dim(pars)[1]) %>% lapply(function(ind){#parallel inside
-  print(ind)
-  freq  = pars[ind,]$freq
-  Amp   = pars[ind,]$Amp
-  p_osc = pars[ind,]$p_osc
-  Nmeas = pars[ind,]$Nmeas
-  stat_method= pars[ind,]$stat_method
+df=c(1:dim(pars)[1]) %>% mclapply(mc.cores=mc_cores,function(ind){#parallel inside
+  freq        = pars[ind,]$freq
+  Amp         = pars[ind,]$Amp
+  p_osc       = pars[ind,]$p_osc
+  Nmeas       = pars[ind,]$Nmeas
+  stat_method = pars[ind,]$stat_method
+  type        = pars[ind,]$type
   
   mt_unif    = c(1:Nmeas)/Nmeas-1/Nmeas
-  mt_opt     = sols[sols@wreg==0 & sols@drts==Inf & sols@Nmeas==Nmeas,]
-  mt_rob     = sols[sols@wreg==1 & sols@drts==6 & sols@Nmeas==Nmeas,]
-  mt_rnc     = sols[sols@wreg==1 & sols@drts==Inf & sols@Nmeas==Nmeas,]
-  mt_opt     = mt_opt[mt_opt<Inf]
-  mt_rob     = mt_rob[mt_rob<Inf]
-  mt_rnc     = mt_rnc[mt_rnc<Inf]
   
   fdr_method=toString(pars[ind,]$fdr_method)
-  if (pars[ind,]$type=='equispaced'){
-    tvec = mt_unif
-  }else if(pars[ind,]$type=='threshold'){
-    tvec = mt_opt
-  }else if(pars[ind,]$type=='balanced'){
-    tvec = mt_rob
-  }else if(pars[ind,]$type=='regu_no_cstr'){
-    tvec = mt_rnc
+  if (type=='equispaced'){
+    mt = c(1:Nmeas)/Nmeas -1/Nmeas 
   }else{
-    stop('unrecognized type')
+    filt = sols@Nmeas==Nmeas & sols@fmin == 1 & sols@fmax==24 & sols@method=='YALMIP'
+    mt = sols[filt,]
+    mt = as.numeric(mt)
+    mt = mt[!is.nan(mt)]
+    if(length(mt)!=Nmeas){
+      stop('wrong length meas vec')
+    }
   }
   
   # simulate data
-  Nmeas               = length(tvec)
+  Nmeas               = length(mt)
   Ydat                = matrix(rnorm(Nmc*Nmeas),nrow=Nmc)
   state               = sample(c('osc','non_osc'),Nmc,replace = T,c(p_osc,1-p_osc))
   N_osc               = sum(state=='osc')
-  Ydat[state=='osc',] = Ydat[state=='osc',]+Amp*cos(outer(2*pi*runif(N_osc),2*pi*freq*tvec,'-'))
+  Ydat[state=='osc',] = Ydat[state=='osc',]+Amp*cos(outer(2*pi*runif(N_osc),2*pi*freq*mt,'-'))
   
   # simualte p-values
   if (stat_method=='cosinor'){
-    pvdf = data.frame(pval=rowCosinor(Ydat,tvec,per=1/freq) %>% {.$pvalue},
+    pvdf = data.frame(pval=rowCosinor(Ydat,mt,per=1/freq) %>% {.$pvalue},
                       state=state)
   }else if(stat_method=='lomb'){
-    pvdf = c(1:dim(Ydat)[1]) %>% mclapply(mc.cores=mc_cores,function(ii){
+    pvdf = c(1:dim(Ydat)[1]) %>% lapply(function(ii){
       x              = Ydat[ii,]
-      lomb_std       = lsp(x,times=tvec,plot=F,normalize = 'standard')
+      lomb_std       = lsp(x,times=mt,plot=F,normalize = 'standard')
       return(data.frame(p_method='std',pval =lomb_std$p.value,state=state[ii]))
     }) %>% rbindlist() %>% data.frame()
   }else{
@@ -62,18 +56,22 @@ df=c(1:dim(pars)[1]) %>% lapply(function(ind){#parallel inside
   }
   
   # FDR correction
-  qval   = p.adjust(pvdf$pval,method=fdr_method)
+  pval   = pvdf$pval
   ostate = pvdf$state
   
   # record AUC
-  roc=pROC::roc(as.numeric(ostate=='osc'),qval,direction='>')
+  roc=pROC::roc(as.numeric(ostate=='osc'),pval,direction='>')
   # record FPR and TPR at alpha=.05
   num_P   = sum(ostate=='osc') 
   num_N   = sum(ostate=='non_osc') 
-  num_TP  = sum(ostate=='osc'     & qval < .05) 
-  num_FP  = sum(ostate=='non_osc' & qval < .05)
+  num_TP  = sum(ostate=='osc'     & pval < .05) 
+  num_FP  = sum(ostate=='non_osc' & pval < .05)
   TPR     = num_TP/num_P
   FPR     = num_FP/num_N
   
   return(cbind(pars[ind,],data.frame(AUC=roc$auc,TPR=TPR,FPR=FPR)))
 }) %>% rbindlist() %>% data.frame()
+
+
+
+df %>% ggplot(aes(x=freq,y=AUC,group=type,color=type))+geom_line()+facet_wrap(~Nmeas)
