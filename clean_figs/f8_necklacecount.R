39746182797081936a8fde2a3bba424fd66bde5d
Turner Silverthorne <turner.silverthorne@gmail.com>
2024-09-30 15:54:05 -0400

    Minor

--- clean_figs/f8_necklacecount.R
+++ clean_figs/f8_necklacecount.R
@@ -11,12 +11,7 @@ Cmat = data.frame(Cmat)
 colnames(Cmat)=c('60 minutes','30 minutes','15 minutes')
 Cmat$Nmeas = rownames(Cmat)
 
-<<<<<<< HEAD
-Cmat = Cmat %>% pivot_longer(cols=c('60 minutes','30 minutes','15 minutes'),names_to='grid',values_to='count')
-Cmat$Nmeas=as.numeric(Cmat$Nmeas)
-Cmat %>% ggplot(aes(x=Nmeas,y=count,group=grid,fill=grid))+geom_line()+scale_y_continuous(trans='log10')+geom_point()
 
-=======
 Cmat$Nmeas = as.numeric(Cmat$Nmeas)
 Cmat = Cmat %>% pivot_longer(cols=c('60 minutes','30 minutes','15 minutes'),
                              names_to='grid',values_to='count')
@@ -32,7 +27,6 @@ scale_x_continuous(breaks=seq(4,20,4))+
        color='grid spacing')
 plt=plt+clean_theme()
 show_temp_plt(plt,6,2)
->>>>>>> 2f02a1a31a0359148bb25329cf4a9e68faeba910
 
 ggsave(paste0('~/research/ms_powerCHORD/figures/',
               'fig_necklace.png'),
