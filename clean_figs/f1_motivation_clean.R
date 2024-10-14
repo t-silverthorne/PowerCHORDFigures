27a401dcf9f12933e6f664a40b27cbb644639feb
Turner Silverthorne <turner.silverthorne@gmail.com>
2024-10-14 08:05:33 -0400

    Minor aesthetic change

--- clean_figs/f1_motivation_clean.R
+++ clean_figs/f1_motivation_clean.R
@@ -24,7 +24,7 @@ pdf = c(1,6) |> lapply(function(freq){
   Yirr    = Amp*cos(outer(acrovec,2*pi*freq*tirr,'-'))+matrix(rnorm(length(tirr)*Nmc),nrow=Nmc)
 
   df_unif = data.frame(meas='equispaced',acro=acrovec,pval= matrixTests::row_cosinor(Yunif,tunif,1/freq) %>% {.$pvalue}) 
-  df_irr  = data.frame(meas='irregular',acro=acrovec,pval= matrixTests::row_cosinor(Yirr,tirr,1/freq) %>% {.$pvalue}) 
+  df_irr  = data.frame(meas='alternative',acro=acrovec,pval= matrixTests::row_cosinor(Yirr,tirr,1/freq) %>% {.$pvalue}) 
 
     
   df_unif = df_unif[df_unif$pval<.05,]
@@ -33,7 +33,7 @@ pdf = c(1,6) |> lapply(function(freq){
   df_all  = data.frame(meas='true',acro=acrovec,pval=NA)
   df      = rbind(df_unif,df_irr,df_all)
 
-  df$meas = factor(df$meas,levels=c('true','equispaced','irregular'))
+  df$meas = factor(df$meas,levels=c('true','equispaced','alternative'))
   df$freq=freq
   return(df)
 }) |> rbindlist() |> data.frame()
@@ -43,10 +43,10 @@ pdf$cmap_var = paste0(pdf$meas,pdf$freq)
 
 pdf = pdf |> mutate(per_label = ifelse(freq==1,'T = 24 hr','T = 4 hr'))
 cmap_cust = c('true1'=rgb(.05,0.5,.06),
-          'irregular1'=rgb(.05,0.5,.06),
+          'alternative1'=rgb(.05,0.5,.06),
           'equispaced1'=rgb(.05,0.5,.06),
           'true6'=rgb(.05,0.5,.06),
-          'irregular6'=rgb(.05,0.5,.06),
+          'alternative6'=rgb(.05,0.5,.06),
           'equispaced6'=rgb(.81,.06,.13))
 
 rad_brk = c(0,pi,2*pi)
@@ -77,9 +77,10 @@ Nm=12
 tunif   = c(1:Nm)/Nm-1/Nm
 tirr    = tau[Xraw[3,]>1e-12]
 df=rbind(data.frame(time=tunif,type='equispaced'),
-         data.frame(time=tirr,type='irregular'))
+         data.frame(time=tirr,type='alternative'))
 dat_bands = data.frame(start=c(0:12)*2,end=(c(0:12)*2+1))
 head(df)
+df$type = factor(df$type,levels=c('equispaced','alternative'))
 plt = df %>% ggplot(aes(x=24*time,y=1))+geom_point()+
 geom_rect(data=dat_bands,
   aes(xmin=start,xmax=end,ymin=-Inf,ymax=Inf),alpha=.4,
@@ -110,7 +111,7 @@ pars   = expand.grid(Amp=Amps,
                      Nmeas=Nmvec,
                      freq=freqs,
                      acro=acros,
-                     type=c('equispaced','irregular'))
+                     type=c('equispaced','alternative'))
 
 pars$power = c(1:dim(pars)[1]) %>% mclapply(mc.cores=8,function(ii){
   x=pars[ii,]
